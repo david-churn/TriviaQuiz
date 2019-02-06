df7c3bc644783800d35118adc787f27cf8fdf94e
David Churn <dchurn@me.com>
2019-02-06 10:32:44 -0600

    further development

--- trivia.js
+++ trivia.js
@@ -4,22 +4,52 @@
 
 let nextObj = document.getElementById("next");
 let questionObj = document.getElementById("question");
+let answerObj = document.getElementById('answers');
 let textAreaObj = document.getElementById("text-area");
+
 let questionNbr = 0;
+let lastQuestionNbr = 0;
 let questionArr = [];
 
-getQuestions();
-questionObj.innerHTML = questionArr[questionNbr].question;
+getQuestions()
+  .then(() => {
+    postQuestion();
+  })
+
+function postQuestion() {
+// post the question
+  questionObj.innerHTML = questionArr[questionNbr].question;
+
+//remove any old answers
+  while (answerObj.hasChildNodes()) {
+    answerObj.removeChild(answerObj.childNodes[0]);
+  }
+
+// create a list of the potential answers
+  let listObj = document.createElement('li');
+  listObj.innerHTML = questionArr[questionNbr].correct_answer;
+  answerObj.appendChild(listObj);
+
+  questionArr[questionNbr].incorrect_answers.forEach(postGuess);
+  function postGuess (guessStr){
+    let listObj = document.createElement('li');
+    listObj.innerHTML = guessStr;
+    answerObj.appendChild(listObj);
+  }
+}
 
 nextObj.addEventListener('click', function() {
-  if (questionNbr > 9) {
-    getQuestions();
-    questionNbr = 0;
+  if (questionNbr > lastQuestionNbr) {
+    getQuestions()
+    .then(() => {
+      questionNbr = 0;
+      postQuestion();
+    })
     }
   else {
     questionNbr += 1;
+    postQuestion();
   }
-  questionObj.innerHTML = questionArr[questionNbr].question;
 });
 
 function getQuestions() {
@@ -31,6 +61,9 @@ function getQuestions() {
     .then(function(respObj) {
       console.log(respObj);
       questionArr = respObj.results;
+//  This is the last array value of the quantity of questions requested.
+      lastQuestionNbr = questionArr.length - 2;
+      console.log(questionArr);
       return;
     })
 }
